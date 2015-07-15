eac743d778474b8ced9372c8bcf394bd907111e6
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2015-07-15 10:29:52 +0300

    Implemented queue data structure.

--- Chapter 02 - Introduction To Data Structures/Queue/src/Program.java
+++ Chapter 02 - Introduction To Data Structures/Queue/src/Program.java
@@ -1,6 +1,27 @@
 public class Program {
+    private static final int MAX = 16;
 
     public static void main(String[] args) {
-	// write your code here
+        Queue<Integer> queue = new Queue<>();
+
+        for (int i = 0; i < 2 * MAX; i++) {
+            queue.enqueue(i);
+            int itemInFront = queue.dequeue();
+            System.out.print(itemInFront + " ");
+        }
+
+        System.out.println("\n");
+
+        queue.enqueue(123);
+        System.out.println("\nПоглед над най-предния елемент: " + queue.peek());
+
+        queue.dequeue();
+
+        // Това ще причини грешка при изключване, тъй като опашката е празна
+        try {
+            queue.dequeue();
+        } catch (IllegalStateException ise) {
+            System.out.println("\n" + ise.getMessage());
+        }
     }
 }
--- Chapter 02 - Introduction To Data Structures/Queue/src/Queue.java
+++ Chapter 02 - Introduction To Data Structures/Queue/src/Queue.java
@@ -1,15 +1,17 @@
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import java.util.Arrays;
 
 public class Queue<T> {
     private static final int INITIAL_CAPACITY = 16;
 
     private int capacity;
+    private int count;
     private int front;
     private int rear;
     private Object[] items;
 
     public Queue() {
         this.capacity = INITIAL_CAPACITY;
+        this.count = 0;
         this.front = 0;
         this.rear = 0;
         this.items = new Object[this.capacity];
@@ -18,22 +20,55 @@ public class Queue<T> {
     public void enqueue(T item) {
         this.ensureCapacity();
 
-        throw new NotImplementedException();
+        this.items[this.rear] = item;
+        this.count++;
+        this.rear++;
+
+        if (this.rear == this.capacity) {
+            this.rear = 0;
+        }
     }
 
     public T dequeue() {
-        throw new NotImplementedException();
+        if (this.isEmpty()) {
+            throw new IllegalStateException("Cannot dequeue an empty queue.");
+        }
+
+        T item = (T) this.items[this.front];
+
+        this.count--;
+        this.front++;
+
+        if (this.front == this.capacity) {
+            this.front = 0;
+        }
+
+        return item;
+    }
+
+    public T peek() {
+        if (this.isEmpty()) {
+            throw new IllegalStateException("Cannot peek an empty queue.");
+        }
+
+        return (T) this.items[this.front];
     }
 
     public int size() {
-        throw new NotImplementedException();
+        return this.count;
     }
 
     public boolean isEmpty() {
-        throw new NotImplementedException();
+        return this.count == 0;
     }
 
     private void ensureCapacity() {
-        throw new NotImplementedException();
+        // Проверка за препълване
+        if (this.front == this.rear && !this.isEmpty()) {
+            this.capacity *= 2;
+            this.items = Arrays.copyOf(this.items, this.capacity);
+            this.front = 0;
+            this.rear = this.count;
+        }
     }
 }
